08d23d43cf7ef55410806e0ca375ad1aa4eebd03
Anastasiia <na.konsta@gmail.com>
2018-10-12 18:47:28 +0300

    Countries section and carousel are almost ready

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,18 @@
 $(function() {
 
-	// Custom JS
+	$(".tourists").owlCarousel({
+		loop: true,
+		smartSpeed: 700,
+		dots: false,
+		nav: true,
+		navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+		responsiveClass: true,
+		responsive: {
+			0: {
+				items: 1
+			}
+		}
+	});
+
 
 });
